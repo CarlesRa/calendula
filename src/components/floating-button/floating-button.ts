596c01543c924bd0cf0e23f166320d490a07e171
CarlesRa <jcramos8282@gmail.com>
2024-07-11 15:41:40 +0200

    social media bar, first design

--- src/components/floating-button/floating-button.ts
+++ src/components/floating-button/floating-button.ts
@@ -1,5 +1,10 @@
 export function floatingButton(): string {
   return `
-    <i class="bi bi-whatsapp whatsapp-icon"></i>
+    <div class="social-media-container">
+      <i class="bi bi-whatsapp social-media-icon whatsapp"></i>
+      <i class="bi bi-instagram social-media-icon instagram"></i>
+      <i class="bi bi-facebook social-media-icon facebook"></i>
+      <i class="bi bi-twitter-x social-media-icon"></i>
+    </div>
   `;
 }
