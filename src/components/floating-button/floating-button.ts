9ae0c9199e61137b71cf64efcd50aff29d88b1d6
CarlesRa <jcramos8282@gmail.com>
2024-07-12 16:45:33 +0200

    adjust image and remove unused styles

--- src/components/floating-button/floating-button.ts
+++ src/components/floating-button/floating-button.ts
@@ -1,10 +1,10 @@
 export function floatingButton(): string {
   return `
     <div class="social-media-container">
-      <i class="bi bi-whatsapp social-media-icon whatsapp"></i>
-      <i class="bi bi-instagram social-media-icon instagram"></i>
-      <i class="bi bi-facebook social-media-icon facebook"></i>
-      <i class="bi bi-twitter-x social-media-icon"></i>
+      <i class="bi bi-whatsapp social-media-icon whatsapp c-pointer"></i>
+      <i class="bi bi-instagram social-media-icon instagram c-pointer"></i>
+      <i class="bi bi-facebook social-media-icon facebook c-pointer"></i>
+      <i class="bi bi-twitter-x social-media-icon c-pointer"></i>
     </div>
   `;
 }
--- src/components/home/home.ts
+++ src/components/home/home.ts
@@ -5,12 +5,14 @@ export function home(): string {
         <div class="card-transparent">
            <h1>Caléndula</h1>
            <h2>
-              Distribuidor de productos de peluquería y estética. ofreciéndole las principales marcas del sector
-              con la mayor calidad y un trato personalizado
+              Distribuidor de productos de peluquería y estética. Ofrecemos las principales marcas del sector
+              con la mayor calidad y un asesoramiento personalizado
             </h2>
           <div class="card-products">           
             <img src="https://1000logos.net/wp-content/uploads/2020/07/MAC-Cosmetics-Logo.jpg" alt="prduct">                     
-            <img src="https://logonoid.com/images/academie-logo.jpg" alt="prduct">                          
+            <img src="https://logonoid.com/images/academie-logo.jpg" alt="prduct">
+            <img src="https://logonoid.com/images/academie-logo.jpg" alt="prduct">                     
+            <img src="https://1000logos.net/wp-content/uploads/2020/07/MAC-Cosmetics-Logo.jpg" alt="prduct">                     
           </div>
         </div>        
       </article>        
